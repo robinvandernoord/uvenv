e0f6b4ab3e10ae83c3d126825babbd0ed408b564
Robin van der Noord <robinvandernoord@gmail.com>
2024-08-07 14:07:28 +0200

    chore: use --zig for glibc backwards compatibility

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ set -e
 
 rm -rf target/wheels/
 
-maturin build --release --strip --target aarch64-unknown-linux-gnu
-maturin build --release --strip --target x86_64-unknown-linux-gnu
+# use --zig for better cross compatibility
+maturin build --release --strip --target aarch64-unknown-linux-gnu --zig
+maturin build --release --strip --target x86_64-unknown-linux-gnu --zig
 maturin sdist
 
 maturin upload --skip-existing -u __token__ target/wheels/*
